5096c6a4f86d5c85402cf4c10d399f235fa990b3
Adil Shaikh <adil.shaikh@republicworld.com>
2019-09-13 11:57:27 +0530

    cleaned up error handling

--- app/src/main/java/com/shaikh/androidarchitecture/data/repository/base/BaseRepository.kt
+++ app/src/main/java/com/shaikh/androidarchitecture/data/repository/base/BaseRepository.kt
@@ -1,11 +1,11 @@
 package com.shaikh.androidarchitecture.data.repository.base
 
 import com.google.gson.Gson
-import com.shaikh.androidarchitecture.domain.exceptions.ApiException
-import com.shaikh.androidarchitecture.domain.entities.ApiResponseWrapper
-import com.shaikh.androidarchitecture.domain.entities.Result
 import com.shaikh.androidarchitecture.data.retrofit.ApiService
 import com.shaikh.androidarchitecture.data.retrofit.ApiServiceBuilder
+import com.shaikh.androidarchitecture.domain.entities.ApiResponseWrapper
+import com.shaikh.androidarchitecture.domain.entities.Result
+import com.shaikh.androidarchitecture.domain.exceptions.ApiException
 import retrofit2.Response
 
 open class BaseRepository {
@@ -13,27 +13,22 @@ open class BaseRepository {
     open var api: ApiService = ApiServiceBuilder.service
 
     internal fun <T, R> parseResult(response: Response<T>, parser: (T) -> R): Result<R> {
-        val responseBody = response.body()
-        return if (responseBody == null) {
-            val errorString = response.errorBody()?.string()
-            return try {
-                val apiResponseWrapper =
-                    Gson().fromJson(errorString, ApiResponseWrapper::class.java)
-                Result.error(
-                    ApiException(
-                        apiResponseWrapper.error ?: errorString
-                    )
-                )
-            } catch (e: Exception) {
-                Result.error(ApiException("Something went wrong"))
-            }
-        } else {
-            return if (response.isSuccessful && response.body() !== null) {
-                Result.success(parser.invoke(response.body()!!))
-            } else {
-                Result.error(ApiException("Hello"))
-            }
-        }
+        return if (response.isSuccessful && response.body() !== null)
+            Result.success(parser.invoke(response.body()!!))
+        else
+            Result.error(getError(response))
     }
 
+    private fun <T> getError(response: Response<T>): ApiException {
+        val errorString = response.errorBody()?.toString() ?: ""
+        val errorObject = try {
+            Gson().fromJson(errorString, ApiResponseWrapper::class.java)
+        } catch (e: Exception) {
+            ApiResponseWrapper<Any>(
+                status = false,
+                error = "Something went wrong"
+            )
+        }
+        return ApiException(errorObject.error)
+    }
 }
