25ad368ac72e7cb5ac644886651c437c03c66028
Adil Shaikh <adil.shaikh@republicworld.com>
2019-09-13 14:23:26 +0530

    added Imagify class for Image loading

--- app/src/main/java/com/shaikh/androidarchitecture/presentation/ui/UsersListAdapter.kt
+++ app/src/main/java/com/shaikh/androidarchitecture/presentation/ui/UsersListAdapter.kt
@@ -3,10 +3,9 @@ package com.shaikh.androidarchitecture.presentation.ui
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import coil.api.load
-import coil.transform.RoundedCornersTransformation
 import com.shaikh.androidarchitecture.R
 import com.shaikh.androidarchitecture.domain.entities.Users
+import com.shaikh.androidarchitecture.presentation.utilities.Imagify
 import com.shaikh.androidarchitecture.presentation.utilities.inflate
 import kotlinx.android.synthetic.main.adapter_user.view.*
 
@@ -21,9 +20,12 @@ class UsersListAdapter(private val list: List<Users>) :
         val data = list[position]
         holder.itemView.apply {
             tvUsername.text = data.name
-            ivUserProfile.load(data.image) {
-                crossfade(1000)
-                transformations(RoundedCornersTransformation(20f))
+            when (position % 5) {
+                1 -> Imagify.loadCircularImage(ivUserProfile, data.image)
+                2 -> Imagify.loadRoundedCorners(ivUserProfile, data.image, 15f)
+                3 -> Imagify.loadBlurImage(ivUserProfile, data.image)
+                4 -> Imagify.loadGreyScale(ivUserProfile, data.image)
+                else -> Imagify.loadImage(ivUserProfile, data.image)
             }
         }
     }
