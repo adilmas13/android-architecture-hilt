968c57863ad0459c772440e23b1cc4fcbfb71b86
Adil Shaikh <adilmas13@gmail.com>
2020-08-10 22:12:34 +0530

    chore : cleanup

--- app/src/main/kotlin/com/androidarchitecture/ArchitectureApplication.kt
+++ app/src/main/kotlin/com/androidarchitecture/ArchitectureApplication.kt
@@ -4,15 +4,4 @@ import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ArchitectureApplication : MultiDexApplication() {
-
-    init {
-        instance = this
-    }
-
-    companion object {
-        private var instance: ArchitectureApplication? = null
-
-        fun getContext() = instance?.applicationContext!!
-    }
-}
+class ArchitectureApplication : MultiDexApplication()
