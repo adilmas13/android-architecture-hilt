53a6bbf33b0e9cee225443100b21adc7310ff16e
Adil Shaikh <adil@invideo.io>
2020-09-10 21:16:56 +0530

    chore: cleanup

--- app/src/main/kotlin/com/androidarchitecture/helpers/ViewModelCreator.kt
+++
@@ -1,5 +0,0 @@
-package com.androidarchitecture.helpers
-
-import androidx.lifecycle.ViewModelProvider
-
-data class ViewModelCreator<M>(val type: Class<M>, val factory: ViewModelProvider.Factory? = null)
