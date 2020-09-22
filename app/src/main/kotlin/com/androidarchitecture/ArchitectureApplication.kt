a8822a656d56a720fa2ad6b8f31091e48ae6c771
Adil Shaikh <adil@invideo.io>
2020-09-22 23:10:12 +0530

    feat: added dark theme support

--- app/src/main/kotlin/com/androidarchitecture/ArchitectureApplication.kt
+++ app/src/main/kotlin/com/androidarchitecture/ArchitectureApplication.kt
@@ -1,7 +1,13 @@
 package com.androidarchitecture
 
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ArchitectureApplication : MultiDexApplication()
+class ArchitectureApplication : MultiDexApplication(){
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+}
