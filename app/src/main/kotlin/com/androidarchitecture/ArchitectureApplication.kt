7c12e014e84bafedb62a97413f2f54486bd86fb6
Adil Shaikh <adil@invideo.io>
2020-09-23 00:36:01 +0530

    feat: set default theme to light mode

--- app/src/main/kotlin/com/androidarchitecture/ArchitectureApplication.kt
+++ app/src/main/kotlin/com/androidarchitecture/ArchitectureApplication.kt
@@ -1,7 +1,14 @@
 package com.androidarchitecture
 
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ArchitectureApplication : MultiDexApplication()
+class ArchitectureApplication : MultiDexApplication(){
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
