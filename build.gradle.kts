149bf0773698667d3fc0aa5f7af47fc8a95fe0ad
Adil Shaikh <adil@invideo.io>
2020-09-22 21:55:04 +0530

    feat: ktlint wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-/*apply plugin: 'org.jetbrains.dokka'*/
-//
-//plugins{
-//    id("org.jetbrains.dokka")
-//}
 plugins {
     id("org.jetbrains.dokka") version "1.4.0"
 }
@@ -34,4 +29,34 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
     delete (rootProject.buildDir)
+}
+
+val ktlint by configurations.creating
+
+dependencies {
+    ktlint("com.pinterest:ktlint:0.39.0")
+    // ktlint(project(":custom-ktlint-ruleset")) // in case of custom ruleset
+}
+
+val outputDir = "${project.buildDir}/reports/ktlint/"
+val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+
+val ktlintCheck by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("src/**/*.kt")
+}
+
+val ktlintFormat by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("-F", "src/**/*.kt")
 }
