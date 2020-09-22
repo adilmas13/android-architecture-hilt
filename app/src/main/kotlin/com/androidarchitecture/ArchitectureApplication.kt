be001a9efb33679746bc7d0d5e0684b6c8533afd
Adil Shaikh <adil@invideo.io>
2020-09-22 23:27:06 +0530

    feat: toggle theme

--- app/src/main/kotlin/com/androidarchitecture/ArchitectureApplication.kt
+++ app/src/main/kotlin/com/androidarchitecture/ArchitectureApplication.kt
@@ -1,13 +1,7 @@
 package com.androidarchitecture
 
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ArchitectureApplication : MultiDexApplication(){
-    override fun onCreate() {
-        super.onCreate()
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-    }
-}
+class ArchitectureApplication : MultiDexApplication()
--- app/src/main/kotlin/com/androidarchitecture/ui/home/MainActivity.kt
+++ app/src/main/kotlin/com/androidarchitecture/ui/home/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.androidarchitecture.ui.home
 
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import com.androidarchitecture.R
 import com.androidarchitecture.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<MainActivityViewModel>() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.theme -> toggleTheme()
+        else -> super.onOptionsItemSelected(item)
+    }
+
+
+    private fun toggleTheme(): Boolean {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        return true
+    }
 
     override fun createViewModel() = MainActivityViewModel::class.java
 
